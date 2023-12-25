e63305e2c2f7374dbff840c6a777f37d2c2fcef4
Prem Chavhan <pchavhan@logiquad.com>
2023-12-25 15:44:51 +0530

    Updated fixes

--- tests/backend/build.sh
+++ tests/backend/build.sh
@@ -4,6 +4,7 @@ sed -i '7d' package.json
 ./manage.sh build-backend-bundle
 rsync -avr --delete ./bundles/backend/ ./docker/images/bundle-backend/
 rm -f ./docker/images/docker-compose.yaml
+mv ./docker/images/build.sh ./docker/images/build-docker.sh
 cp -rf ./docker/images/* ./
 mv Dockerfile.backend Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/penpot-backend:latest | docker load
