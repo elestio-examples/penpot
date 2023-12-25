40fa718d6aa283354b90b6e61b8ed966d88406be
Prem Chavhan <pchavhan@logiquad.com>
2023-12-25 16:00:45 +0530

    Updated fixes

--- tests/exporter/build.sh
+++ tests/exporter/build.sh
@@ -4,6 +4,7 @@ sed -i '7d' package.json
 ./manage.sh build-exporter-bundle
 rsync -avr --delete ./bundles/exporter/ ./docker/images/bundle-exporter/
 rm -f ./docker/images/docker-compose.yaml
+mv ./docker/images/build.sh ./docker/images/build-docker.sh
 cp -rf ./docker/images/* ./
 mv Dockerfile.exporter Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/penpot-exporter:latest | docker load
--- tests/frontend/build.sh
+++ tests/frontend/build.sh
@@ -4,6 +4,7 @@ sed -i '7d' package.json
 ./manage.sh build-frontend-bundle
 rsync -avr --delete ./bundles/frontend/ ./docker/images/bundle-frontend/
 rm -f ./docker/images/docker-compose.yaml
+mv ./docker/images/build.sh ./docker/images/build-docker.sh
 cp -rf ./docker/images/* ./
 mv Dockerfile.frontend Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/penpot-frontend:latest | docker load
