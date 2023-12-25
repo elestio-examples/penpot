33ef7ea68aefff79971230dfdccc8948fe38c562
Prem Chavhan <pchavhan@logiquad.com>
2023-12-25 16:57:18 +0530

    Updated fixes

--- tests/backend/build.sh
+++ tests/backend/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
+set -o allexport; source .env; set +o allexport
 sed -i '7d' package.json
+sed -i "s~--mount source=`pwd`~--mount source=${folderName}~g" ./manage.sh
 ./manage.sh build-backend-bundle
 rsync -avr --delete ./bundles/backend/ ./docker/images/bundle-backend/
 rm -f ./docker/images/docker-compose.yaml
--- tests/exporter/build.sh
+++ tests/exporter/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
+set -o allexport; source .env; set +o allexport
 sed -i '7d' package.json
+sed -i "s~--mount source=`pwd`~--mount source=${folderName}~g" ./manage.sh
 ./manage.sh build-exporter-bundle
 rsync -avr --delete ./bundles/exporter/ ./docker/images/bundle-exporter/
 rm -f ./docker/images/docker-compose.yaml
