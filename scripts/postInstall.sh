93fe0b52a720abcd0f3e1b35a71ad5a340ad4506
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-07-01 08:49:23 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -4,5 +4,5 @@ set -o allexport; source .env; set +o allexport;
 echo "Creating user..."
 sleep 120
 
-docker-compose exec -T penpot-backend ./manage.py create-profile -e ${ADMIN_EMAIL} -p ${ADMIN_PASSWORD} -n Root
+docker-compose exec -T penpot-backend ./manage.py create-profile -e ${ADMIN_EMAIL} -p ${ADMIN_PASSWORD} -n root
 docker-compose exec -T penpot-backend ./manage.py update-profile -e ${ADMIN_EMAIL} -p ${ADMIN_PASSWORD} -n root
